0d4a5f3e1090c5f79fe1aba560305ec1dbb0df1c
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-04 13:13:48 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/zapxcero/basicprog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zapxcero.basicprog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(title: Text("Hello World")),
+  )));
+}
