30b3b82af79e7be6731ee080b105e9006c48fdaa
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-04 20:52:04 +0800

    Convert register_screen to Stateful

--- lib/Screens/register_screen.dart
+++ lib/Screens/register_screen.dart
@@ -1,14 +1,88 @@
 import 'package:basicprog/Widgets/background_image_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import '../firebase_options.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Stack(
-        children: [BackgroundImage(imagePath: 'assets/login_register_bg.png')],
+        children: [
+          const BackgroundImage(imagePath: 'assets/login_register_bg.png'),
+          FutureBuilder(
+            future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 30),
+                      const Text('Register'),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: _email,
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(hintText: 'Email'),
+                      ),
+                      const SizedBox(height: 30),
+                      TextField(
+                        controller: _password,
+                        obscureText: true,
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        decoration: const InputDecoration(hintText: 'Password'),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final email = _email.text;
+                          final password = _password.text;
+
+                          final userCredential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          print(userCredential);
+                        },
+                        child: const Text('Register'),
+                      ),
+                    ],
+                  );
+                default:
+                  return const Center(child: Text('Loading...'));
+              }
+            },
+          ),
+        ],
       ),
     );
   }
