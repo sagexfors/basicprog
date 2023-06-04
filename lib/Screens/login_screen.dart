ffd3485d8559dd599ea855c513cd8ee1883a5d9c
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-04 20:52:23 +0800

    Add LoginScreen

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,8 +1,33 @@
 import 'package:basicprog/Widgets/background_image_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import '../firebase_options.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +35,52 @@ class LoginScreen extends StatelessWidget {
       body: Stack(
         children: [
           const BackgroundImage(imagePath: 'assets/login_register_bg.png'),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 30),
-              const Text('Welcome'),
-              const SizedBox(height: 20),
-              const TextField(),
-              const SizedBox(height: 30),
-              const TextField(),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Log in'),
-              ),
-            ],
+          FutureBuilder(
+            future: Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.done:
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 30),
+                      const Text('Login'),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: _email,
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(hintText: 'Email'),
+                      ),
+                      const SizedBox(height: 30),
+                      TextField(
+                        controller: _password,
+                        obscureText: true,
+                        autocorrect: false,
+                        enableSuggestions: false,
+                        decoration: const InputDecoration(hintText: 'Password'),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final email = _email.text;
+                          final password = _password.text;
+
+                          final userCredential = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email, password: password);
+                          print(userCredential);
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ],
+                  );
+                default:
+                  return const Center(child: Text('Loading...'));
+              }
+            },
           ),
         ],
       ),
