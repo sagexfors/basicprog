2e7da008d65d74b8a77756e68c7766bd7283a3d4
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-04 14:29:11 +0800

    Add placeholder screens

---
+++ lib/Screens/about_us_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AboutUsScreen extends StatelessWidget {
+  const AboutUsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Us'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:basicprog/Screens/home_screen.dart';
+import 'package:basicprog/Screens/login_screen.dart';
+import 'package:basicprog/Screens/register_screen.dart';
+import 'package:basicprog/Screens/about_us_screen.dart';
+import 'package:basicprog/Screens/authentication_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(title: const Text("Hello World")),
-  )));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/about-us': (context) => const AboutUsScreen(),
+        '/auth': (context) => const AuthScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+      },
+    );
+  }
 }
