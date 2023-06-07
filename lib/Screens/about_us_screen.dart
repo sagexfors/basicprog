56922c67e95bfa9ec052828f27d288459b5587cd
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-08 07:21:34 +0800

    Add About Us Screen

--- lib/Screens/about_us_screen.dart
+++ lib/Screens/about_us_screen.dart
@@ -7,7 +7,18 @@ class AboutUsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About Us'),
+        title: const Text('About Us'),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/about_us_bg.png'),
+                  fit: BoxFit.cover),
+            ),
+          )
+        ],
       ),
     );
   }
