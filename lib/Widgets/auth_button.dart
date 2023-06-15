04a5ae8ad4af8cfe2325c517eaf580d1c0ad37d2
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-15 12:31:00 +0800

    Resize button

--- lib/Widgets/auth_button.dart
+++ lib/Widgets/auth_button.dart
@@ -2,20 +2,39 @@ import 'package:flutter/material.dart';
 
 class AuthButton extends StatelessWidget {
   final VoidCallback onPressed;
-
   final String name;
 
   const AuthButton({
-    super.key,
+    Key? key,
     required this.onPressed,
     required this.name,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(name),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: double.infinity, // Make the button as wide as possible
+        height: 50, // Adjust the desired height
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(10), // Adjust the desired border radius
+            ),
+          ),
+          child: Text(
+            name,
+            style: const TextStyle(
+              letterSpacing: 2,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
