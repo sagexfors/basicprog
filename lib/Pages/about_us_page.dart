696bf207ce852d4ff415ce65fc6934ee52a3e987
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-16 10:41:55 +0800

    Change about_us temp img

--- lib/Pages/about_us_page.dart
+++ lib/Pages/about_us_page.dart
@@ -6,12 +6,13 @@ class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('About Us')),
       body: Stack(
         children: [
           Container(
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/about_us_bg.png'),
+                image: AssetImage('assets/images/temp_about_us.png'),
                 fit: BoxFit.cover,
               ),
             ),
