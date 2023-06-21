78e65e2ec715b1c17e2dcfc4b03a3db240b22be8
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-21 21:43:21 +0800

    Remake About Us

--- lib/Pages/about_us_page.dart
+++ lib/Pages/about_us_page.dart
@@ -6,17 +6,63 @@ class AboutUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('About Us')),
-      body: Stack(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('About Us'),
+      ),
+      body: Column(
         children: [
           Container(
+            height: 200,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/temp_about_us.png'),
-                fit: BoxFit.cover,
+                image: AssetImage('assets/images/about_us.png'),
+                fit: BoxFit.fitWidth,
               ),
             ),
-          )
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'About Basic: ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(width: 90),
+              Container(
+                width: 120,
+                height: 120,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/about_us_extra.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Card(
+            elevation: 5,
+            child: Padding(
+              padding: EdgeInsets.all(36.0),
+              child: Text(
+                'Basic; is an application that contains activities,quizzes,lessons in learning programming languages. the user will learn and develop their logical skills in answering the questions on the system. The purpose of this application is to improve their comprehension in coding and programming.',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         ],
       ),
     );
