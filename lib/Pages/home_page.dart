c01ae2303469eab45a854ce825044dccae87c10b
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-21 21:43:08 +0800

    Wrap body with SingleChildScrollView

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -479,58 +479,60 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 96),
-                Container(
-                  width: 170,
-                  height: 170,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/home.png'),
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 96),
+                  Container(
+                    width: 170,
+                    height: 170,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/home.png'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 32),
-                const Text(
-                  'Welcome to Basic',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 90, 96, 179),
+                  const SizedBox(height: 32),
+                  const Text(
+                    'Welcome to Basic',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 90, 96, 179),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24),
-                const Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    '"The art of programming is the skill of controlling complexity." -Marijn Haverbeke',
-                    style: TextStyle(fontSize: 15),
-                    textAlign: TextAlign.center,
+                  const SizedBox(height: 24),
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      '"The art of programming is the skill of controlling complexity." -Marijn Haverbeke',
+                      style: TextStyle(fontSize: 15),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 120),
-              ],
+                  const SizedBox(height: 120),
+                ],
+              ),
             ),
-          ),
-          const Positioned(
-            top: -80,
-            left: -100,
-            child: CircleThing(opacity: 0.7),
-          ),
-          const Positioned(
-            top: -140,
-            left: -10,
-            child: CircleThing(opacity: 0.7),
-          ),
+            const Positioned(
+              top: -80,
+              left: -100,
+              child: CircleThing(opacity: 0.7),
+            ),
+            const Positioned(
+              top: -140,
+              left: -10,
+              child: CircleThing(opacity: 0.7),
+            ),
 
-          // LoginForm(showRegisterPage: widget.showRegisterPage)
-        ],
+            // LoginForm(showRegisterPage: widget.showRegisterPage)
+          ],
+        ),
       ),
       drawer: const NavigationDrawer(),
     );
