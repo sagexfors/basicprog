cc0f52693e294ab1c0eb7a0de25d90b1626724d3
Adrian Rodriguez <zapxcero@gmail.com>
2023-06-28 21:40:32 +0800

    Use material3

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => const GettingStartedPage(),
