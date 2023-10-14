05b3caca14782960496c5561d02518652f93e316
sage <zapxcero@gmail.com>
2023-10-14 10:20:54 +0800

    add compiler page and codexapi service that wasn't added before for some reason

---
+++ lib/Pages/compiler_page.dart
@@ -0,0 +1,75 @@
+import 'package:basicprog/services/codexapi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_code_editor/flutter_code_editor.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
+import 'package:highlight/languages/cpp.dart';
+
+class CompilerPage extends StatefulWidget {
+  const CompilerPage({super.key});
+
+  @override
+  State<CompilerPage> createState() => _CompilerPageState();
+}
+
+class _CompilerPageState extends State<CompilerPage> {
+  final controller = CodeController(
+    text: '''
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+''', // Initial co
+    language: cpp,
+  );
+  var output = '';
+  var error = '';
+
+  final TextEditingController _inputController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Code Editor'),
+      ),
+      body: Center(
+        child: CodeTheme(
+          data: CodeThemeData(styles: monokaiSublimeTheme),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _inputController,
+                  decoration: const InputDecoration(
+                    hintText: 'Input',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    var response = await CodexApiService.compileCode(
+                      controller.text,
+                      _inputController.text,
+                    );
+                    output = response['output'];
+                    error = response['error'];
+                    print(output);
+                    print(error);
+                  },
+                  child: const Text('Run'),
+                ),
+                CodeField(
+                  controller: controller,
+                  minLines: 28,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: ADD TAB WITH OUTPUT  RESULT and proper error display
---
+++ lib/services/codexapi.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class CodexApiService {
+  static const String apiUrl = 'https://api.codex.jaagrav.in';
+
+  static Future<Map<String, dynamic>> compileCode(
+    String code,
+    String input,
+  ) async {
+    final url = Uri.parse(apiUrl);
+    final headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+    final data = {'code': code, 'language': 'c', 'input': input};
+    final encodedData = Uri(queryParameters: data).query;
+
+    try {
+      final response =
+          await http.post(url, headers: headers, body: encodedData);
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        return {
+          'output': jsonResponse['output'] ?? '',
+          'error': jsonResponse['error'] ?? '',
+        };
+      } else {
+        throw Exception('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to the API: $e');
+    }
+  }
+}
