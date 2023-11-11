308e1e88ddac5c7a5cda2d6d4e14288f30589bcc
sagexfors <47491562+sagexfors@users.noreply.github.com>
2023-11-11 12:26:04 +0800

    extract routes to constants folder

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,11 @@
 import 'package:basicprog/Pages/getting_started.dart';
 import 'package:basicprog/auth/main_page.dart';
+import 'package:basicprog/constants/routes.dart';
 import 'package:basicprog/pages/activities_page.dart';
 import 'package:basicprog/pages/change_password_page.dart';
 import 'package:basicprog/pages/compiler_page.dart';
 import 'package:basicprog/pages/forgot_password_page.dart';
-import 'package:basicprog/pages/lessons_page.dart';
+import 'package:basicprog/pages/lessons/lessons_page.dart';
 import 'package:basicprog/pages/profile_page.dart';
 import 'package:basicprog/provider/lessons_provider.dart';
 import 'package:basicprog/provider/user_provider.dart';
@@ -33,21 +34,21 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/': (context) => const GettingStartedPage(),
-          '/about-us': (context) => const AboutUsPage(),
-          '/main': (context) => const MainPage(),
-          '/profile': (context) => const ProfilePage(),
-          '/lessons': (context) => Consumer<LessonsProvider>(
+          gettingStartedRoute: (context) => const GettingStartedPage(),
+          aboutUsRoute: (context) => const AboutUsPage(),
+          mainRoute: (context) => const MainPage(),
+          profileRoute: (context) => const ProfilePage(),
+          lessonsRoute: (context) => Consumer<LessonsProvider>(
                 builder: (context, value, child) {
                   return LessonsPage(
                     lessons: value.lessons,
                   );
                 },
               ),
-          '/activities': (context) => const ActivitiesPage(),
-          '/compiler': (context) => const CompilerPage(),
-          '/change-password': (context) => const ChangePasswordPage(),
-          '/forgot-password': (context) => const ForgotPasswordPage(),
+          activitiesRoute: (context) => const ActivitiesPage(),
+          compilerRoute: (context) => const CompilerPage(),
+          changePasswordRoute: (context) => const ChangePasswordPage(),
+          forgotPasswordRoute: (context) => const ForgotPasswordPage(),
         },
       ),
     );
