b53ebc19fe08b17c0fcecfcb6fc8f267db7595ed
sagexfors <47491562+sagexfors@users.noreply.github.com>
2023-11-18 11:29:54 +0800

    improved quizzes page

--- lib/Pages/quizzes_page.dart
+++ lib/Pages/quizzes_page.dart
@@ -1,6 +1,5 @@
 import 'package:basicprog/Pages/quiz_page.dart';
 import 'package:flutter/material.dart';
-
 import '../model/quiz.dart';
 
 class QuizzesPage extends StatelessWidget {
@@ -41,32 +40,67 @@ class QuizCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
     return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Theme.of(context).primaryColor,
-          child: Text(
-            quizNumber.toString(),
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        title: Text(quiz.title),
-        subtitle: quiz.description == '' ? null : Text(quiz.description),
-        trailing: const Icon(Icons.arrow_forward),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(8.0),
+      elevation: 4,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
         onTap: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => QuizPage(
-                quiz: quiz,
-              ),
+              builder: (context) => QuizPage(quiz: quiz),
             ),
           );
         },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildLeading(theme),
+              const SizedBox(width: 16),
+              Expanded(child: _buildQuizInfo(theme, quiz)),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildLeading(ThemeData theme) {
+    return Icon(
+      Icons.quiz, // Quiz-related icon
+      size: 30,
+      color: theme.colorScheme.primary,
+    );
+  }
+
+  Widget _buildQuizInfo(ThemeData theme, Quiz quiz) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          quiz.title,
+          style: theme.textTheme.titleLarge,
+          overflow: TextOverflow.visible,
+          softWrap: true, // Allows long titles to wrap
+        ),
+        const SizedBox(height: 4),
+        quiz.description == ''
+            ? Container()
+            : Text(
+                quiz.description,
+                style: theme.textTheme.titleSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+      ],
     );
   }
 }
