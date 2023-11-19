57fb2d0b9fbb413d3a8ab2f557dd08b7a5989113
sagexfors <47491562+sagexfors@users.noreply.github.com>
2023-11-19 19:16:55 +0800

    update todo

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,22 @@ void main() async {
 
 //TODO: store quiz scores in users collections (user id, quiz id, score)
 //TODO: store assessment scores in users collections (user id, assessment id, score)
+//TODO: which means i have to fetch the quizzes and assessment again with the score and then build them again with the score if it exists.
+// in other words
+// 1. fetch the quizzes/assessment
+// 2. fetch the scores from users collection
+// 3. build the quizzes/assessment with the scores if they exist
+// structure for storing scores in users collection
+
+  // users
+  //   - user id
+  //     - quizzes
+  //       - quiz id
+  //         - score
+  //     - assessments
+  //       - assessment id
+  //         - score
+  //     - lessons
+  //       - lesson id
+  //         - completed
+
