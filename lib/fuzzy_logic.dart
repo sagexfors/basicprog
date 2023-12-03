e33e7adbbc10586bf54a08eb1040bbf528abddc1
sagexfors <47491562+sagexfors@users.noreply.github.com>
2023-12-03 14:45:03 +0800

    improve fuzzy logic

--- lib/fuzzy_logic.dart
+++ lib/fuzzy_logic.dart
@@ -1,29 +1,44 @@
-Map<String, double> fuzzyRuleEvaluation(double score) {
-  double low(double x) => (x >= 25 && x <= 50) ? (50 - x) / 25 : 0;
-  double medium(double x) => (x >= 25 && x <= 75)
-      ? 1
-      : (x >= 75 && x <= 100)
-          ? (x - 75) / 25
-          : 0;
-  double high(double x) => (x >= 75 && x <= 100) ? (x - 75) / 25 : 0;
+// Adjusted membership functions for better granularity and representation
+double low(double x) => (x <= 30)
+    ? 1
+    : (x > 30 && x < 50)
+        ? (50 - x) / 20
+        : 0;
+double medium(double x) => (x > 40 && x < 60)
+    ? (x - 40) / 20
+    : (x >= 60 && x <= 80)
+        ? (80 - x) / 20
+        : 0;
+double high(double x) => (x >= 70) ? (x - 70) / 30 : 0;
 
-  Map<String, double> fuzzyResult = {};
-  fuzzyResult['Pass'] = medium(score) * high(score);
-  fuzzyResult['Fail'] = low(score) * medium(score);
+// Modified fuzzy rule evaluation to account for more nuanced scenarios
+Map<String, double> fuzzyRuleEvaluation(double score) {
+  Map<String, double> fuzzyResult = {
+    'HighPass': high(score),
+    'LowPass': medium(score) * high(score),
+    'Uncertain': medium(score),
+    'LowFail': low(score) * medium(score),
+    'HighFail': low(score),
+  };
   return fuzzyResult;
 }
 
+// Enhanced defuzzification function for more accurate results
 double defuzzification(Map<String, double> fuzzyResult) {
   double numerator = 0;
   double denominator = 0;
+  Map<String, double> weights = {
+    'HighPass': 100,
+    'LowPass': 75,
+    'Uncertain': 50,
+    'LowFail': 25,
+    'HighFail': 0,
+  };
+
   fuzzyResult.forEach((key, value) {
-    if (key == 'Pass') {
-      numerator += value * 100;
-    } else {
-      numerator += value * 0;
-    }
+    numerator += value * weights[key]!;
     denominator += value;
   });
-  double centroid = (denominator != 0) ? numerator / denominator : 0;
-  return centroid;
+
+  return (denominator != 0) ? numerator / denominator : 0;
 }
